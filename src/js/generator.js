26214c40c86e10ced560e1f9c840b7ea6b9b7a76
ashwin <ashwinkn@hotmail.com>
2018-12-28 20:51:53 -0600

    Made code a little more terse and easy to understand

--- src/js/generator.js
+++ src/js/generator.js
@@ -6,41 +6,48 @@ import PATH from 'path';
 import lodash from 'lodash';
 
 class Generator {
-    constructor(design = undefined, map = undefined, overrideOptions = undefined) {
-        this._options = new Options(process.argv.slice(2), overrideOptions);
+    _loadOneDesign(design, designFile) {
+        const augmentedDesign = JSON.parse(FS.readFileSync(designFile));
+        return { ...design, ...augmentedDesign };
+    }
 
-        // Load design files
-        let rawDesign = this.options.design.reduce((design, designFile) => {
-            const designJSON = JSON.parse(FS.readFileSync(designFile));
-            return { ...design, ...designJSON };
-        }, {});
+    _loadDesign(design) {
+        design = design || {};
 
-        // merge input design
-        rawDesign = design ? { ...rawDesign, ...design } : rawDesign;
+        // Load design files and overwrite with input design
+        let rawDesign = { ...this.options.design.reduce((...args) => this._loadOneDesign(...args), {}), ...design };
 
         // Normalize design
         this._design = getDesign(rawDesign);
+    }
 
+    _normalizeMapEntry(mapFile, map, [fileName, templateDescription]) {
+        const templateFile = templateDescription.template;
+        let template = PATH.relative(PATH.resolve('./'), PATH.resolve(PATH.dirname(mapFile), templateFile));
+        if (PATH.isAbsolute(templateFile)) template = templateFile;
+        map[fileName] = { ...templateDescription, template };
+        return map;
+    }
+
+    _loadOneMap(map, mapFile) {
+        let mapFileText = FS.readFileSync(mapFile);
+        let newMap = JSON.parse(lodash.template(mapFileText)(this.design));
+
+        // Fixup all file names to global names
+        newMap = Object.entries(newMap).reduce((...args) => this._normalizeMapEntry(mapFile, ...args), {});
+
+        return { ...map, ...newMap };
+    }
+
+    _loadMaps(map) {
         // Load map files
-        this._map = this.options.map.reduce((map, mapFile) => {
-            let newMap = FS.readFileSync(mapFile);
-            newMap = JSON.parse(lodash.template(newMap)(this.design));
-
-            // Fixup all file names to global names
-            newMap = Object.entries(newMap).reduce((map, [fileName, templateDescription]) => {
-                const templateFile = templateDescription.template;
-                let template = PATH.relative(PATH.resolve('./'), PATH.resolve(PATH.dirname(mapFile), templateFile));
-                if (PATH.isAbsolute(templateFile)) template = templateFile;
-                map[fileName] = { ...templateDescription, template };
-                return map;
-            }, {});
-
-            return { ...map, ...newMap };
-        }, {});
+        this._map = this.options.map.reduce((...args) => this._loadOneMap(...args), {});
 
         // merge maps
         this._map = map ? { ...this.map, ...map } : this.map;
+    }
 
+    _checkErrors() {
         if (Object.keys(this.map).length === 0) {
             throw new Error('No maps found');
         }
@@ -48,6 +55,13 @@ class Generator {
         if (Object.keys(this.design).length === 0) {
             throw new Error('No designs found');
         }
+    }
+
+    constructor(design = undefined, map = undefined, overrideOptions = undefined) {
+        this._options = new Options(process.argv.slice(2), overrideOptions);
+        this._loadDesign(design);
+        this._loadMaps(map);
+        this._checkErrors();
     }
 
     get design() {
