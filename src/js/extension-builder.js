1e88586ed49e6ef51afd9898f6443729af0c2a17
ashwin <ashwinkn@hotmail.com>
2019-02-14 14:02:18 -0600

    Fixed ext generation

--- src/js/extension-builder.js
+++ src/js/extension-builder.js
@@ -48,7 +48,7 @@ class ExtensionBuilder {
             .reverse();
     }
 
-    getExtensions(match) {
+    getExtensions(match, labels) {
         return this.extensions
             .filter(ext => ext.name.match(match))
             .map(ext => {
@@ -56,7 +56,7 @@ class ExtensionBuilder {
                     logger.info(`loading extension ${PATH.relative(PATH.resolve('./'), PATH.resolve(ext.file))}`);
                     ext.extension = new Extension(ext.file, this.generator).load();
                 }
-                return ext.extension.text;
+                return ext.extension.generate(labels);
             });
     }
 
