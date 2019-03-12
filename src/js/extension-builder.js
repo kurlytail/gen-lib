e030a18dc538bd13d92286d4379479e1f74682bc
ashwin <ashwinkn@hotmail.com>
2019-03-12 18:02:28 -0500

    Fixed up flow

--- src/js/extension-builder.js
+++ src/js/extension-builder.js
@@ -1,14 +1,24 @@
+// @flow
+
 import FS from 'fs';
 import Extension from './extension';
 import PATH from 'path';
 import logger from './logger';
 
 class ExtensionBuilder {
-    constructor(generator) {
+
+    // eslint-disable-next-line flowtype/no-weak-types
+    _generator: Object;
+    // eslint-disable-next-line flowtype/no-weak-types
+    _extensions: Array<Object>
+
+    // eslint-disable-next-line flowtype/no-weak-types
+    constructor(generator: Object) {
         this._generator = generator;
     }
 
-    _loadOneExtension(extensions, extension) {
+    // eslint-disable-next-line flowtype/no-weak-types
+    _loadOneExtension(extensions: Array<Object>, extension: string): Array<Object> {
         const stat = FS.statSync(extension);
 
         /* Convert file and directories to extensions */
@@ -23,35 +33,39 @@ class ExtensionBuilder {
         /* Assume file is a directory */
         if (stat.isDirectory()) {
             const files = FS.readdirSync(extension).filter(
-                file => file.match(/.*\._$/) && FS.statSync(`${extension}/${file}`).isFile()
+                (file: string): boolean => !!file.match(/.*\._$/) && FS.statSync(`${extension}/${file}`).isFile()
             );
-            extensions = files.reduce((extensions, file) => {
-                return this._loadOneExtension(extensions, `${extension}/${file}`);
-            }, extensions);
+            // eslint-disable-next-line flowtype/no-weak-types
+            extensions = files.reduce((extensions: Array<Object>, file: string): Array<Object> =>
+                this._loadOneExtension(extensions, `${extension}/${file}`), extensions);
 
-            const directories = FS.readdirSync(extension).filter(dir => {
+            const directories = FS.readdirSync(extension).filter((dir: string): boolean => {
                 return FS.statSync(`${extension}/${dir}`).isDirectory();
             });
-            extensions = directories.reduce((extensions, dir) => {
-                return this._loadOneExtension(extensions, `${extension}/${dir}`);
-            }, extensions);
+            // eslint-disable-next-line flowtype/no-weak-types
+            extensions = directories.reduce((extensions: Array<Object>, dir: string): Array<Object> =>
+                this._loadOneExtension(extensions, `${extension}/${dir}`), extensions);
             return extensions;
         }
 
-        throw new Error(`Unrecognized extension ${extension} ${stat.isFile()}`);
+        throw new Error(`Unrecognized extension ${extension}`);
     }
 
     build() {
         /* reverse the list so that the last extension is seen first */
         this._extensions = this.generator.options.extension
-            .reduce((...args) => this._loadOneExtension(...args), [])
+            // eslint-disable-next-line flowtype/no-weak-types
+            .reduce((...args: any): Array<Object> => this._loadOneExtension(...args), [])
             .reverse();
     }
 
-    getExtensions(match, labels, templateDescription) {
+    // eslint-disable-next-line flowtype/no-weak-types
+    getExtensions(match: string, labels: Array<string>, templateDescription: Object): Array<string> {
         return this.extensions
-            .filter(ext => ext.name.match(match))
-            .map(ext => {
+            // eslint-disable-next-line flowtype/no-weak-types
+            .filter((ext: Object): boolean => ext.name.match(match))
+            // eslint-disable-next-line flowtype/no-weak-types
+            .map((ext: Object): string => {
                 if (!ext.extension) {
                     logger.info(`loading extension ${PATH.relative(PATH.resolve('./'), PATH.resolve(ext.file))}`);
                     ext.extension = new Extension(ext.file, this.generator).load();
@@ -60,16 +74,18 @@ class ExtensionBuilder {
             });
     }
 
-    get generator() {
+    // eslint-disable-next-line flowtype/no-weak-types
+    get generator(): Object {
         return this._generator;
     }
 
-    get extensions() {
+    // eslint-disable-next-line flowtype/no-weak-types
+    get extensions(): Array<Object> {
         return this._extensions;
     }
 
-    toString() {
-        return this.extensions;
+    toString(): string {
+        return this.extensions.toString();
     }
 }
 
